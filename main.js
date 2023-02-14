766e99d2fc1f759560a26a40c793f0ec337785d7
siraj-akmal <akmal.s@northeastern.edu>
2023-02-14 16:41:55 -0500

    button adds the point

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+var e = document.getElementById("framework1");
+var text = e.options[e.selectedIndex].text;
+console.log(text);
 function buttonClicked() {
   console.log("button was clicked!");
 
@@ -8,5 +10,21 @@ function buttonClicked() {
   buttonDiv.innerHTML = concat;
 }
 
-  
+
+// adds new points based on input
+function submitPoint() {
+    // gets new coordinate points
+    let xInput = document.getElementById("framework1");
+    var xOutput = xInput.options[xInput.selectedIndex].text;
+    let xCoor = (xOutput * 40) + 5
+    let yInput = document.getElementById("framework2");
+    var yOutput = yInput.options[yInput.selectedIndex].text;
+    let yCoor = ((10 - yOutput) * 40) + 5
+    console.log(xCoor);
+    console.log(yCoor);
+    let graph = document.getElementById("graph");
+    graph.innerHTML += "<circle class='point' cx='" + xCoor + "' cy='" + yCoor + "' r='10'></circle>"
+}
+
+
 
