8aaa8ab59fb3e79c30e77e950f84648a1745e5b5
vivekkanpa <122316072+vivekkanpa@users.noreply.github.com>
2023-02-14 11:27:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function buttonClicked() {
+  console.log("button was clicked!");
+
+  // select the element
+  let buttonDiv = document.getElementById("button-div");
+
+  buttonDiv.innerHTML = concat;
+}
+
+  
+
